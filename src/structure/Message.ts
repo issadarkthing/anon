a14cb287961d0e23f35f30f69147d90e75d8f485
raziman <raziman@tutanota.com>
2023-02-26 18:36:44 +0800

    remove time field from payload body

--- src/structure/Message.ts
+++ src/structure/Message.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 const MAX_STRING = 1024;
 
 export const messageSchema = z.object({
-  time: z.string().max(MAX_STRING).datetime(),
   message: z.string().max(MAX_STRING),
 });
 
